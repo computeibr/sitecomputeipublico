40368ff31d6eb51e60bffb84fecb9fcd3ff93ecc
Leonardo Pires <leonardo.computei@gmail.com>
2023-11-14 19:39:59 -0300

    senha_acesso

---
+++ js/acesso.js
@@ -0,0 +1,15 @@
+var numeroSecreto = "3221@"
+
+// parseInt(Math.random() * 1001);
+
+while(chute != numeroSecreto) {
+  var chute = prompt('Digite o acesso')
+  //se o chute for igual ao número secreto 
+  if (chute == numeroSecreto) {
+    alert('Acesso Autorizado!')
+  } else if (chute > numeroSecreto) {
+    alert('Não Autorizado... ')
+  } else if (chute < numeroSecreto) {
+    alert('Não Autorizado... ')
+  }
+}
